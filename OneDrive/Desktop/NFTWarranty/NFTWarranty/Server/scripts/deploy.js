0f6063e753d845b08e041c3635dce6c3e52039cd
csn2002 <csnaruka76@gmail.com>
2022-08-05 23:30:10 +0530

    NFT CREATION

---
+++ OneDrive/Desktop/NFTWarranty/NFTWarranty/Server/scripts/deploy.js
@@ -0,0 +1,18 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+//0x271D2BeB5db7266Cd34D512C8F8c06AD247fab4a
+
+// 0x30D80727581f8EF90d28212B4aF2DEA85eF9b7Ce
+
---
+++ OneDrive/Desktop/NFTWarranty/NFTWarranty/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Navbar from './components/Navbar'
+import Alert from './components/Alert'
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    
+    </>
+  );
+}
+
+export default App;
